0559b7aba4c65a8606c3f4f61c32b2c7155dc60a
Hassan <hanconicoff@gmail.com>
2026-08-14 12:27:29 +0300

    Create supplier and supplier location database structure

---
+++ database/migrations/2026_08_14_082637_create_suppliers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('suppliers', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('user_id')
+            ->unique()
+            ->constrained()
+            ->cascadeOnDelete();
+
+        $table->string('business_name');
+        $table->string('tin_number')->unique();
+        $table->text('description')->nullable();
+
+        $table->enum('status', [
+            'pending',
+            'approved',
+            'suspended',
+        ])->default('pending');
+
+        $table->timestamp('verified_at')->nullable();
+
+        $table->timestamps();
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
